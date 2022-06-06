c236dec2d6dd52e1e56dde49db42ad9ff7a930c9
iamarobot-a <36001407+iamarobot-a@users.noreply.github.com>
2022-06-06 14:49:12 +0200

    Lint, remove duplicate vars

--- avatar.py
+++ avatar.py
@@ -3,33 +3,29 @@ import requests
 from PIL import Image
 
 #CONFIG
-userId="<YOUR ZOOM EMAIL>"
+userId = "<YOUR ZOOM EMAIL>"
 #token expires on ....
-jwtToken="<YOUR JWT TOKEN>"
+jwtToken = "<YOUR JWT TOKEN>"
 
-backgroundUrl="https://picsum.photos/200/243/?random" #bg should be same size or bigger than fg
-foregroundFile="./foreground200x243.png"
-backgroundFile="./.background.jpg"
-mergedFile="./.merged.jpg"
+backgroundUrl = "https://picsum.photos/200/243/?random" #bg should be same size or bigger than fg
+foregroundFile = "./foreground200x243.png"
+backgroundFile = "./.background.jpg"
+mergedFile = "./.merged.jpg"
 
 def get_image():
-    global backgroundUrl
-    imgURL=backgroundUrl
-    request.urlretrieve(imgURL, "./.background.jpg")
+    request.urlretrieve(backgroundUrl, backgroundFile)
 
 def merge_images():
     background = Image.open(backgroundFile)
     foreground = Image.open(foregroundFile)
-    height_diff=background.size[1]-foreground.size[1]
+    height_diff = background.size[1]-foreground.size[1]
     background.paste(foreground, (0, height_diff), foreground)  # this moves the fg to the bottom of the bg
     background.save(mergedFile, "JPEG", optimize=True, quality=80)
     
 def upload_merged():
-    token=jwtToken
-
     url = 'https://api.zoom.us/v2/users/{0}/picture'.format(userId)
 
-    headers = {'Authorization': 'Bearer {}'.format(token),
+    headers = {'Authorization': 'Bearer {}'.format(jwtToken),
             'Accept': 'application/json',
             }
     
